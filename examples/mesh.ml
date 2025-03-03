fd05580848d732cbaf7411e619b3aa55e70b9dac
J.Stark <rho.ajax@gmail.com>
2025-03-03 16:59:37 +0100

    editing...

--- examples/mesh.ml
+++ examples/mesh.ml
@@ -50,3 +50,24 @@ let _ =
   Plot.subplot h 1 2;
   Plot.(mesh ~h ~spec:[ Azimuth 115.; ZLine Y; NoMagColor; RGB (130,255,40) ] x y z);
   Plot.output h
+
+
+let _ = 
+  let x, y = Mat.meshgrid (-3.) 3. (-3.) 3. 100 100 in
+  let z = Mat.(
+    3. $* ((1. $- x) **$ 2.) * exp (neg (x **$ 2.) - ((y +$ 1.) **$ 2.)) -
+    (10. $* (x /$ 5. - (x **$ 3.) - (y **$ 5.)) * (exp (neg (x **$ 2.) - (y **$ 2.)))) -
+    ((1./.3.) $* exp (neg ((x +$ 1.) **$ 2.) - (y **$ 2.)))
+    )
+  in
+
+  let h = Plot.create ~m:2 ~n:2 "plot_017.png" in
+  Plot.subplot h 0 0;
+  Plot.(mesh ~h ~spec:[ Contour ] x y z);
+  Plot.subplot h 0 1;
+  Plot.heatmap ~h x y z;
+  Plot.subplot h 1 0;
+  Plot.mesh ~h x y z;
+  Plot.subplot h 1 1;
+  Plot.contour ~h x y z;
+  Plot.output h
